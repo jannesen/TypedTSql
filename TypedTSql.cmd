2a49276a8b83392eab1774a2c76b0096db22c562
Peter Jannesen <peter@jannesen.com>
2019-01-04 23:59:31 +0100

    Add cmd script for debugging a project

---
+++ TypedTSql.cmd
@@ -0,0 +1,3 @@
+set TypedTSqlBuild=%~dp0Jannesen.VisualStudioExtension.TypedTSql.Build\bin\
+
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv.exe" TypedTSql.sln
