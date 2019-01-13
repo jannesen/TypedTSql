4cd7b10215e10ee813f48fb8ee7400d08683b679
Peter Jannesen <peter@jannesen.com>
2019-01-13 17:44:45 +0100

    Better tracing of project changes for languageservice.

--- Jannesen.VisualStudioExtension.TypedTSql/LanguageService/TextBufferLanguageServiceProject.cs
+++ Jannesen.VisualStudioExtension.TypedTSql/LanguageService/TextBufferLanguageServiceProject.cs
@@ -6,25 +6,73 @@ namespace Jannesen.VisualStudioExtension.TypedTSql.LanguageService
 {
     internal class TextBufferLanguageServiceProject
     {
-        public      readonly        string                              FilePath;
-        public      readonly        Project                             LanguageService;
+        private     readonly        IServiceProvider                    _serviceProvider;
+        private     readonly        ITextBuffer                         _textBuffer;
+        private                     Project                             _languageService;
+        private                     Project.SourceFile                  _sourceFile;
+
+        public                      ITextBuffer                         TextBuffer
+        {
+            get {
+                return _textBuffer;
+            }
+        }
+        public                      string                              FilePath
+        {
+            get {
+                return _textBuffer.Properties.GetProperty<ITextDocument>(typeof(ITextDocument)).FilePath;
+            }
+        }
+        public                      Project                             LanguageService
+        {
+            get
+            {
+                lock(this) {
+                    _updateLink();
+
+                    if (_languageService == null) {
+                        throw new InvalidOperationException("File is not part of TypedTSql project.");
+                    }
+
+                    return _languageService;
+                }
+            }
+        }
 
         private                                                         TextBufferLanguageServiceProject(IServiceProvider serviceProvider, ITextBuffer textBuffer)
         {
-            this.FilePath     = textBuffer.Properties.GetProperty<ITextDocument>(typeof(ITextDocument)).FilePath;
-
-            var project = VSPackage.GetContainingProject(CPS.TypedTSqlUnconfiguredProject.ProjectTypeGuid, FilePath);
-            if (project != null) {
-                LanguageService = serviceProvider.GetService<Service>(typeof(Service))
-                                                 .GetLanguageService(project);
-                LanguageService.TextBufferConnected(this);
-            }
+            _serviceProvider = serviceProvider;
+            _textBuffer      = textBuffer;
         }
 
         public      static          TextBufferLanguageServiceProject    GetLanguageServiceProject(IServiceProvider serviceProvider, ITextBuffer textBuffer)
         {
-            var tbls = textBuffer.Properties.GetOrCreateSingletonProperty<TextBufferLanguageServiceProject>(typeof(TextBufferLanguageServiceProject), () => new TextBufferLanguageServiceProject(serviceProvider, textBuffer));
-            return tbls.LanguageService != null ? tbls : null;
+            return textBuffer.Properties.GetOrCreateSingletonProperty<TextBufferLanguageServiceProject>(typeof(TextBufferLanguageServiceProject), () => new TextBufferLanguageServiceProject(serviceProvider, textBuffer));
+        }
+
+        internal                    FileResult                          GetFileResult()
+        {
+            lock(this)
+            {
+                _updateLink();
+                return _sourceFile != null && _sourceFile.TextBuffer == _textBuffer ? _sourceFile.Result : null;
+            }
+        }
+
+        private                     void                                _updateLink()
+        {
+            if (_languageService == null || _sourceFile == null || _sourceFile.Project != _languageService) {
+                var filePath = FilePath;
+                var project = VSPackage.GetContainingProject(CPS.TypedTSqlUnconfiguredProject.ProjectTypeGuid, filePath);
+                if (project != null) {
+                    _languageService = _serviceProvider.GetService<Service>(typeof(Service))
+                                                       .GetLanguageService(project);
+                    _sourceFile = _languageService.TextBufferConnected(this);
+                }
+                else {
+                    System.Diagnostics.Debug.WriteLine("ERROR: " + filePath + " not part of a project");
+                }
+            }
         }
     }
 }
