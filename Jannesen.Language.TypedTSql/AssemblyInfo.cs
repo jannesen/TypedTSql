6ebf38c8130ea515aab65ec5bf59cdaf6052f15f
Peter Jannesen <peter@jannesen.com>
2019-09-13 00:16:21 +0200

    Bumpup version to 1.09.01.004

--- Jannesen.Language.TypedTSql/AssemblyInfo.cs
+++ Jannesen.Language.TypedTSql/AssemblyInfo.cs
@@ -9,5 +9,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark        ("")]
 [assembly: AssemblyCulture          ("")]
 [assembly: ComVisible               (false)]
-[assembly: AssemblyVersion          ("1.09.01.003")] /*@VERSIONINFO*/
-[assembly: AssemblyFileVersion      ("1.09.01.003")] /*@VERSIONINFO*/
+[assembly: AssemblyVersion          ("1.09.01.004")] /*@VERSIONINFO*/
+[assembly: AssemblyFileVersion      ("1.09.01.004")] /*@VERSIONINFO*/
