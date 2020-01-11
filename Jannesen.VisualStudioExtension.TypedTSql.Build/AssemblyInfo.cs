101181a62a1892cc270d83d49aa45836d8fb9936
Peter Jannesen <peter@jannesen.com>
2020-01-11 12:51:20 +0100

    bumpup version to 1.09.04.000

--- Jannesen.VisualStudioExtension.TypedTSql.Build/AssemblyInfo.cs
+++ Jannesen.VisualStudioExtension.TypedTSql.Build/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture          ("")]
 [assembly: ComVisible               (false)]
 [assembly: Guid                     ("091F7030-6776-45E3-B0FA-DBED1EDFF4D0")]
-[assembly: AssemblyVersion          ("1.09.04.000")]      //@VERSIONINFO
-[assembly: AssemblyFileVersion      ("1.09.04.000")]  //@VERSIONINFO
+[assembly: AssemblyVersion          ("1.09.05.000")]      //@VERSIONINFO
+[assembly: AssemblyFileVersion      ("1.09.05.000")]  //@VERSIONINFO
