2dd6e6742049d6173a22d318a32b57f846728a8c
Peter Jannesen <peter@jannesen.com>
2025-12-18 12:20:44 +0100

    Add ROW_NUMBER

---
+++ Jannesen.Language.TypedTSql/BuildIn/Ranking/ROW_NUMBER.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Jannesen.Language.TypedTSql.Node;
+using Jannesen.Language.TypedTSql.Logic;
+
+namespace Jannesen.Language.TypedTSql.BuildIn.Func
+{
+    // https://learn.microsoft.com/en-us/sql/t-sql/functions/row-number-transact-sq
+    public class ROW_NUMBER: ExprCalculationBuildIn
+    {
+        public      readonly    Node_OVER                           n_Over;
+
+        public      override    DataModel.ValueFlags                ValueFlags          => DataModel.ValueFlags.Function;
+        public      override    DataModel.ISqlType                  SqlType             => DataModel.SqlTypeNative.BigInt;
+
+        internal                                                    ROW_NUMBER(Internal.BuildinFunctionDeclaration declaration, Core.ParserReader reader): base(declaration, reader)
+        {
+            ParseToken(reader, Core.TokenID.LrBracket);
+            ParseToken(reader, Core.TokenID.RrBracket);
+            AddChild(n_Over = new Node_OVER(reader));
+        }
+
+        public      override    void                                TranspileNode(Transpile.Context context)
+        {
+            try {
+                n_Over.TranspileNode(context);
+            }
+            catch(Exception err) {
+                context.AddError(this, err);
+            }
+        }
+    }
+}
---
+++ Jannesen.Language.TypedTSql/Node/Node/Node_OVER.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Jannesen.Language.TypedTSql.Library;
+
+namespace Jannesen.Language.TypedTSql.Node
+{
+    // https://msdn.microsoft.com/en-us/library/ms188385.aspx
+    public class Node_OVER: Core.AstParseNode
+    {
+        public      readonly    Expr_with_COLLATE[]             n_PartitionItems;
+        public      readonly    Expr_with_COLLATE[]             n_OrderByItems;
+        
+        public      static      bool                            CanParse(Core.ParserReader reader)
+        {
+            return reader.CurrentToken.isToken("OVER") && reader.NextPeek().isToken(Core.TokenID.GROUP);
+        }
+        public                                                  Node_OVER(Core.ParserReader reader)
+        {
+            ParseToken(reader, "OVER");
+            ParseToken(reader, Core.TokenID.LrBracket);
+
+            if (ParseOptionalToken(reader, "PARTITION") != null) {
+                ParseToken(reader, Core.TokenID.BY);
+                n_PartitionItems = ParseItems(reader, (r) => new Expr_with_COLLATE(r));
+            }
+
+            ParseToken(reader, Core.TokenID.ORDER);
+            ParseToken(reader, Core.TokenID.BY);
+            n_OrderByItems = ParseItems(reader, (r) => new Expr_with_COLLATE(r));
+            ParseToken(reader, Core.TokenID.RrBracket);
+        }
+
+        public      override    void                            TranspileNode(Transpile.Context context)
+        {
+            n_PartitionItems?.TranspileNodes(context);
+            n_OrderByItems?.TranspileNodes(context);
+        }
+    }
+}
